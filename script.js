15f4913478957ce09af95264096d1f2fb3359ac6
cpbattrick <cpbattrick@googlemail.com>
2019-07-30 08:38:43 -0400

    added  jsrender and made suggested corrections

--- script.js
+++ script.js
@@ -1,38 +1,49 @@
+function renderTile(container, letter) {
+  let template = $.templates("#tile-template");
+  template.link(container, { letter: letter });
+}
+
 $(document).ready(function() {
   // Function to request a word from server
-  const getWord = () => {
+  function getWord() {
     return $.ajax({
       method: "GET",
       url: "http://tom.mo2.dinksurveys.net/api/1.0/getWord?letters=5",
       dataType: "json"
     });
-  };
+  }
 
   // Function to get a word, add letters to it, randomise order
   // and then render the letters on the page.
-  const renderWord = () => {
+  function renderWord() {
     getWord().done(function(data) {
       $("#response").text("");
       $("#word").hide();
       $("#word").text(data.word);
       const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-      data.word += characters.charAt(
-        Math.floor(Math.random() * characters.length)
-      );
-      data.word += characters.charAt(
-        Math.floor(Math.random() * characters.length)
-      );
+
+      for (let i = 0; i < 2; ++i) {
+        data.word += characters.charAt(
+          Math.floor(Math.random() * characters.length)
+        );
+      }
+
       let wordArray = data.word.split("");
 
       for (let i = wordArray.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]];
       }
-      $.each(wordArray, function(i, letter) {
-        $(`#tileBottom${i}`).text(letter);
+
+      $(".tileBottom").each(function(i, e) {
+        renderTile($(e), wordArray[i]);
       });
+
+      //   $.each(wordArray, function(i, letter) {
+      //     // $(`#tileBottom${i}`).text(letter);
+      //   });
     });
-  };
+  }
 
   // Initial call to render word on page load
   renderWord();
@@ -40,9 +51,9 @@ $(document).ready(function() {
   // Onclick funcionality for bottom row
   $(".tileBottom").click(function(e) {
     for (let i = 0; i < 7; ++i) {
-      if ($(`#tileTop${i}`).text() === "") {
+      if ($(`#tileTop${i}`).text() == "") {
         $(`#tileTop${i}`).text(e.currentTarget.innerText);
-        i += 8;
+        break;
       }
     }
     e.currentTarget.innerText = "";
@@ -51,9 +62,9 @@ $(document).ready(function() {
   // Onclick functionality for top row
   $(".tileTop").click(function(e) {
     for (let i = 0; i < 7; ++i) {
-      if ($(`#tileBottom${i}`).text() === "") {
+      if ($(`#tileBottom${i}`).text() == "") {
         $(`#tileBottom${i}`).text(e.currentTarget.innerText);
-        i += 8;
+        break;
       }
     }
     e.currentTarget.innerText = "";
@@ -81,7 +92,7 @@ $(document).ready(function() {
         url: `http://tom.mo2.dinksurveys.net/api/1.0/isword?word=${answer}`,
         dataType: "json"
       }).done(function(data) {
-        if (data.isWord === false) {
+        if (!data.isWord) {
           $("#response").text("Try Again!");
         } else {
           $("#response").text("Well Done!");
